e18468fdb8ea14f828352fe589d1d5c4d49e1430
Jan Rabe <jan.rabe@exozet.com>
2015-12-03 18:15:18 +0100

    added stubs

---
+++ Unity/Stubs/AndroidJavaClassStub.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Platform.Android.Stubs
+{
+	public class AndroidJavaClassStub  {
+
+		public void Call(params object [] method) {
+
+		}
+	}
+}
---
+++ Unity/Stubs/AndroidJavaObjectStub.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Platform.Android.Stubs{
+
+	public class AndroidJavaObjectStub {
+
+		public void Call(params object [] method) {
+
+		}
+	}
+}
